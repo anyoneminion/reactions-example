82a188d7d4241b84030696b63bef8ac1eab517d5
kinomori <anyone6879@gmail.com>
2022-03-10 15:05:00 -0500

    settings change

--- settings.lua
+++ settings.lua
@@ -1,24 +1,24 @@
 local tbl = 
 {
-	{
+	fakeSettingCheckbox1 = {
 		type = "checkbox",
 		label = "fake setting name",
 		tooltip = "fake hover tooltip",
 		bool = false,
 	},
-	{
+	fakeSettingCheckbox2 = {
 		type = "checkbox",
 		label = "fake setting name 2",
 		tooltip = "fake hover tooltip 2",
 		bool = true,
 	},
-	{
+	fakeSettingNumber1 = {
 		type = "number",
 		label = "fake number setting name",
 		tooltip = "fake number hover tooltip",
 		num = 50,
 	},
-	{
+	fakeSettingNumber2 = {
 		type = "number",
 		label = "fake number setting name 2",
 		tooltip = "fake number hover tooltip 2 ",
